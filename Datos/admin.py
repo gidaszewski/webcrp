77da0756a9c2bbccf87709bccd924754ca63eb20
Franco Gidaszewski <francogidaszewski@gmail.com>
2023-04-04 20:38:09 -0300

    -

--- Datos/admin.py
+++ Datos/admin.py
@@ -4,7 +4,7 @@ from Datos.models import *
 # Register your models here.
 
 class ArchivoAdmin(admin.ModelAdmin):
-    list_display = ["nombre", "apellido", "distancia", "archivo"]
-    search_fields = ["nombre" "apellido", "distancia"]
+    list_display = ["nombre", "apellido", "distancia", "email", "archivo"]
+    search_fields = ["nombre", "apellido", "distancia", "email"]
 
 admin.site.register(Archivo, ArchivoAdmin)
--- Registro/admin.py
+++ Registro/admin.py
@@ -10,7 +10,7 @@ class UsuarioAdmin(ExportActionMixin, admin.ModelAdmin):
     resource_class = UsuarioResource
     list_display = ["nombre", "apellido", "fecha_de_nacimiento", "dni","distancia", "pais", "estado", "genero", "telefono", "talle_de_remera","categoria","email"]
     list_editable = ["talle_de_remera", "telefono", "email", "distancia"]
-    search_fields = ["nombre" "apellido", "dni", "talle_de_remera",]
+    search_fields = ["nombre", "apellido", "dni", "talle_de_remera",]
 
 
 admin.site.register(Usuario, UsuarioAdmin)
