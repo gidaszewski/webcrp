ec6aa6212c1eec0f6c480bbf0916d9666317da3d
Franco Gidaszewski <francogidaszewski@gmail.com>
2023-04-04 19:32:17 -0300

    -

--- Pago/urls.py
+++ Pago/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('pagar', MetodosDePago, name="pagar"),
     path('pagar/descuento', aplicar_cupon, name="pagar-descuento"),
     path('pagar/descuento/valido', mostrar_descuento, name="pagar-descuento-valido"),
-    path('pagar/descuento/valido', mostrar_super_descuento, name="pagar-descuento-valido-30"),
+    path('pagar/super_descuento/valido', mostrar_super_descuento, name="pagar-descuento-valido-30"),
 ]
