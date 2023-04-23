c9a058f42f27f3f1062d29d9903cdbffe8fe0c3b
Franco Gidaszewski <francogidaszewski@gmail.com>
2023-04-23 11:48:39 -0300

    -

--- Registro/urls.py
+++ Registro/urls.py
@@ -4,7 +4,7 @@ from Registro.views import *
 
 
 urlpatterns = [
-    path('registro/', Usuario_Inscripsion, name="registro"),
+    path('registro_soldout/', Usuario_Inscripsion, name="registro"),
     path('completado/', Completado, name="completado"),
     path('descargar-pdf/', descargar_pdf, name="descargar_pdf"),
 ]
