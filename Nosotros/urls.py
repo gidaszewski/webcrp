3f26aee1254dbf8df1b0f89dba2369a2db523641
Franco Gidaszewski <francogidaszewski@gmail.com>
2024-01-14 20:20:45 -0300

    -

--- Nosotros/urls.py
+++ Nosotros/urls.py
@@ -1,7 +1,1 @@
-from django.contrib import admin
-from django.urls import path
-from Nosotros.views import *
 
-urlpatterns = [
-    path('nosotros/', Sobre_Nostros, name='sobre-nosotros')
-]
--- Nosotros/views.py
+++ Nosotros/views.py
@@ -1,5 +1,1 @@
 from django.shortcuts import render
-
-# Create your views here.
-def Sobre_Nostros(request):
-    return render(request, 'Nosotros/Sobre_Nosotros.html')
