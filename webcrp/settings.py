03fb4563aa7996f7ed0746cb370d877946c8a1f6
Franco Gidaszewski <francogidaszewski@gmail.com>
2024-01-14 19:36:43 -0300

    -

--- webcrp/settings.py
+++ webcrp/settings.py
@@ -141,9 +141,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-
 STATIC_URL = "/static/"
-STATIC_ROOT = "Home/static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
 
