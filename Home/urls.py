6bea54e46a3ebd2db867ef80564a045bd4684979
gidaszewski <gidaszewskifranco@gmail.com>
2024-05-22 02:32:53 +0200

    -

--- Home/urls.py
+++ Home/urls.py
@@ -1,14 +1,7 @@
 from django.urls import path
 from Home.views import *
-from django.conf import settings
-from django.conf.urls.static import static
 
 
 urlpatterns = [
     path("", home, name="home"),
-    path("carrera-de-la-mujer/", info_evento_actual, name="info-evento-actual"),
-    path("chana-challenge/", info_chana, name="info-chana"),
-    path("mantenimiento/", mantenimiento, name="mantenimiento"),
-    path("sobre-nosotros/", about_us, name="sobre-nosotros"),
-    path("galeria/", galery, name="galeria"),
 ]
--- Home/views.py
+++ Home/views.py
@@ -1,38 +1,6 @@
 from django.shortcuts import render
-import json
-import os
-from django.conf import settings
 
 
 # Create your views here.
 def home(request):
-    return render(request, "Home/home.html")
-
-
-def info_evento_actual(request):
-    return render(request, "Home/carrera_de_la_mujer_info.html")
-
-
-def info_chana(request):
-    return render(request, "Home/chana_challenge.html")
-
-
-def mantenimiento(request):
-    return render(request, "Home/mantenimiento.html")
-
-
-def about_us(request):
-    return render(request, "Home/nosotros.html")
-
-
-def galery(request):
-    json_path = os.path.join(settings.STATIC_ROOT, "Home", "images.json")
-
-    with open(json_path) as f:
-        images_json = json.load(f)
-
-    context = {
-        "images": images_json["images"],
-    }
-
-    return render(request, "Home/galeria.html", context)
+    return render(request, "Home/index.html")
