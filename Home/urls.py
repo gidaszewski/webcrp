ae98cd3e39d78938c1cf5ce3f42b7628c355a2d9
gidaszewski <gidaszewskifranco@gmail.com>
2024-06-28 14:08:34 +0200

    urls: fix

--- Home/urls.py
+++ Home/urls.py
@@ -5,5 +5,5 @@ from Home.views import *
 urlpatterns = [
     path("", home, name="home"),
     path("amanecer/", amanecer, name="amanecer"),
-    path("trail-crp/", trail_crp, name="trail-crp"),
+    path("trail-crp/", trail_crp, name="trailcrp"),
 ]
