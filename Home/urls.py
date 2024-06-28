569e35513bd1873a6a940fd256b61043f46800f2
gidaszewski <gidaszewskifranco@gmail.com>
2024-06-28 14:01:52 +0200

    Frontend: nuevas carreras

--- Home/urls.py
+++ Home/urls.py
@@ -4,4 +4,6 @@ from Home.views import *
 
 urlpatterns = [
     path("", home, name="home"),
+    path("amanecer/", amanecer, name="amanecer"),
+    path("trail-crp/", trail_crp, name="trail-crp"),
 ]
--- Home/views.py
+++ Home/views.py
@@ -4,3 +4,11 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, "Home/index.html")
+
+
+def trail_crp(request):
+    return render(request, "Home/trail-crp.html")
+
+
+def amanecer(request):
+    return render(request, "Home/amanecer.html")
